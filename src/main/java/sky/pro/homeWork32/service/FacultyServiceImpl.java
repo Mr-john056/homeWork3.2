ca32827587d0e86fd7369920d9730bd2da4da496
MSI <kulakov056@mail.ru>
2024-01-12 22:59:08 +0500

    filling classes

--- src/main/java/sky/pro/homeWork32/service/FacultyServiceImpl.java
+++ src/main/java/sky/pro/homeWork32/service/FacultyServiceImpl.java
@@ -10,17 +10,17 @@ public class FacultyServiceImpl {
     private long count = 0;
 
 
-    public  Faculty addFaculty(Faculty faculty){
+    public Faculty addFaculty(Faculty faculty) {
         faculty.setId(count++);
         faculties.put(faculty.getId(), faculty);
         return faculty;
     }
 
-    public Faculty findFaculty(long id){
+    public Faculty findFaculty(long id) {
         return faculties.get(id);
     }
 
-    public Faculty editFaculty(long id, Faculty faculty){
+    public Faculty editFaculty(long id, Faculty faculty) {
         if (!faculties.containsKey(id)) {
             return null;
         }
@@ -28,7 +28,7 @@ public class FacultyServiceImpl {
         return faculty;
     }
 
-    public void deleteFaculty(long id){
+    public void deleteFaculty(long id) {
         faculties.remove(id);
     }
 }
--- src/main/java/sky/pro/homeWork32/service/StudentService.java
+++ src/main/java/sky/pro/homeWork32/service/StudentService.java
@@ -1,4 +1,14 @@
 package sky.pro.homeWork32.service;
 
+import sky.pro.homeWork32.entities.Student;
+
 public interface StudentService {
+
+    Student addStudent(Student student);
+
+    Student findStudent(long id);
+
+    Student editStudent(long id, Student student);
+
+    void deleteStudent(long id);
 }
